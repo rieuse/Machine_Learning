300dbdf18b1776b6458f1c11cb97d40d39ddc054
rieuse <rieuse@163.com>
2018-10-22 11:44:21 +0800

    add traffic classfication

--- House_Regression/house_XG.py
+++ House_Regression/house_XG.py
@@ -1,8 +1,8 @@
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
-from sklearn.learning_curve import learning_curve
-from sklearn.metrics import r2_score
+from sklearn.metrics import r2_score,accuracy_score,auc
+from sklearn.model_selection import train_test_split, learning_curve
 from xgboost import XGBRegressor
 
 
@@ -14,39 +14,37 @@ data_X = df[['rooms','halls','size','unit_price']].astype('int')
 data_y = df.price
 
 
-train_sizes, train_score, test_score = learning_curve(
-    XGBRegressor(max_depth=4), data_X, data_y, cv=10, scoring='r2',
-    train_sizes=np.linspace(0.0, 1.0, num=30)[1:])
-
-
-train_score_mean = np.mean(train_score, axis=1)
-test_score_mean = np.mean(test_score, axis=1)
-
-plt.plot(train_sizes, train_score_mean, 'o-', color="r",
-         label="Training")
-plt.plot(train_sizes, test_score_mean, 'o-', color="g",
-        label="validation")
-
-plt.xlabel("Training examples")
-plt.ylabel("Score")
-plt.show()
-
-
-
+# train_sizes, train_score, test_score = learning_curve(
+#     XGBRegressor(max_depth=4), data_X, data_y, cv=10, scoring='r2',
+#     train_sizes=np.linspace(0.0, 1.0, num=30)[1:])
+#
+#
+# train_score_mean = np.mean(train_score, axis=1)
+# test_score_mean = np.mean(test_score, axis=1)
+#
+# plt.plot(train_sizes, train_score_mean, 'o-', color="r",
+#          label="Training")
+# plt.plot(train_sizes, test_score_mean, 'o-', color="g",
+#         label="validation")
+#
+# plt.xlabel("Training examples")
+# plt.ylabel("Score")
+# plt.show()
 
 
 
 
-# scores = []
-# for i in range(1,100):
-#     X_train, X_test, y_train, y_test = train_test_split(
-#         data_X, data_y, test_size=0.3)
-#     reg = XGBRegressor(max_depth=4, learning_rate=0.1)
-#     reg.fit(X_train, y_train)
-#     y_pred = reg.predict(X_test)
-#     score = r2_score(y_pred, y_test)
-#     scores.append(score)
-# print(np.mean(scores))
+scores = []
+for i in range(1,100):
+    X_train, X_test, y_train, y_test = train_test_split(
+        data_X, data_y, test_size=0.3)
+    reg = XGBRegressor(max_depth=4, learning_rate=0.1)
+    reg.fit(X_train, y_train)
+    y_pred = reg.predict(X_test)
+    score = r2_score(y_pred, y_test)
+    # score = accuracy_score(y_pred, y_test)
+    scores.append(score)
+print(np.mean(scores))
 
 
 
---
+++ traffic_classification/traffic_LASSO.py
@@ -0,0 +1,47 @@
+# -*- coding: utf-8 -*-
+
+
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+from sklearn.linear_model import LogisticRegression
+from sklearn.model_selection import learning_curve
+from sklearn.metrics import average_precision_score
+
+train = pd.read_csv("data/train.csv")
+test = pd.read_csv("data/test.csv")
+submit = pd.read_csv("data/sample_submit.csv")
+# 删除id
+train.drop('CaseId', axis=1, inplace=True)
+test.drop('CaseId', axis=1, inplace=True)
+
+# 取出训练集的y
+y_train = train.pop('Evaluation')
+
+# train_sizes, train_score, test_score = learning_curve(
+#     LogisticRegression(), train, y_train, cv=10, scoring='r2',
+#     train_sizes=np.linspace(0.0, 1.0, num=30)[1:])
+#
+# train_score_mean = np.mean(train_score, axis=1)
+# test_score_mean = np.mean(test_score, axis=1)
+#
+# plt.plot(train_sizes, train_score_mean, 'o-', color="r",
+#          label="Training")
+# plt.plot(train_sizes, test_score_mean, 'o-', color="g",
+#         label="validation")
+#
+# plt.xlabel("Training examples")
+# plt.ylabel("Score")
+# plt.show()
+# exit()
+
+# 建立LASSO逻辑回归模型
+
+clf = LogisticRegression(penalty='l1', C=1.0 ,random_state=0)
+clf.fit(train, y_train)
+y_pred = clf.predict_proba(test)[:, 1]
+
+
+# 输出预测结果至my_LASSO_prediction.csv
+submit['Evaluation'] = y_pred
+submit.to_csv('data/my_LASSO_prediction.csv', index=False)
